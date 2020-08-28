896cdf0b6591ae50d401e7865bac439bd69eb397
h_yonemori <h_yonemori@euphonic.co.jp>
2020-08-28 16:15:32 +0900

    yonemori

---
+++ Assets/Scripts/RandomMatchmaker.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomMatchmaker : MonoBehaviour
+{
+    public GameObject photonObject;
+
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings("0.1");
+    }
+
+    void OnJoinedLobby()
+    {
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    void OnPhotonRandomJoinFailed()
+    {
+        PhotonNetwork.CreateRoom(null);
+    }
+
+    void OnJoinedRoom()
+    {
+        PhotonNetwork.Instantiate(
+            photonObject.name,
+            new Vector3(0f, 1f, 0f),
+            Quaternion.identity, 0
+        );
+
+        GameObject mainCamera =
+            GameObject.FindWithTag("MainCamera");
+        mainCamera.GetComponent<UnityChan.ThirdPersonCamera>().enabled = true;
+    }
+
+    void OnGUI()
+    {
+        GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString());
+    }
+}
---
+++ Assets/VoiceDemo.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VoiceDemo : MonoBehaviour
+{
+
+    private string cubeName = "Cube";
+
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings("0.1");
+    }
+
+    void OnJoinedLobby()
+    {
+        RoomOptions options = new RoomOptions()
+        {
+            isVisible = false,
+            maxPlayers = 4
+        };
+        PhotonNetwork.JoinOrCreateRoom("room", options, TypedLobby.Default);
+    }
+
+    void OnJoinedRoom()
+    {
+        PhotonNetwork.Instantiate(
+            cubeName,
+            Vector3.zero,
+            Quaternion.identity,
+            0
+        );
+    }
+
+}
